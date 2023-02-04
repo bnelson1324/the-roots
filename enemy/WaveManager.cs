77557aaa82941916a066b8447776ca8eff0f426a
bnelson1324 <33214860+bnelson1324@users.noreply.github.com>
2023-02-04 14:01:20 -0800

    implemented wave indicator

--- enemy/WaveManager.cs
+++ enemy/WaveManager.cs
@@ -6,8 +6,8 @@ namespace roottowerdefense.enemy;
 
 public partial class WaveManager : Node
 {
-    private int _timeBetweenEnemies = 4;
-    private int _timeBetweenWaves = 8;
+    private float _timeBetweenEnemies = 4;
+    private float _timeBetweenWaves = 8;
     [Export] private PackedScene _trashEnemy;
 
     private Timer _enemyTimer;
@@ -17,6 +17,10 @@ public partial class WaveManager : Node
     private int _wavePackIndex;
     private int _wavePackEnemiesSpawned;
     private WaveEvent[] _wavesList;
+
+    // wave indicator
+    private int _currentWaveIndicatorNum = 1;
+    private int _totalWaves;
 
     public override void _Ready()
     {
@@ -34,14 +38,26 @@ public partial class WaveManager : Node
             new Wave(new WavePack(_trashEnemy, 4)),
             new WaveLambda(() => { _timeBetweenEnemies = 3; }),
             new Wave(new WavePack(_trashEnemy, 8)),
+            new WaveLambda(() => { _timeBetweenEnemies = 2.5f; }),
             new Wave(new WavePack(_trashEnemy, 10)),
             new WaveLambda(() => { _timeBetweenEnemies = 1; }),
             new Wave(new WavePack(_trashEnemy, 12)),
+            new WaveLambda(() => { _timeBetweenEnemies = 0.2f; }),
+            new Wave(new WavePack(_trashEnemy, 12)),
         };
+
+        // calc totalWaves
+        foreach (var waveEvent in _wavesList)
+        {
+            if (waveEvent is Wave)
+                _totalWaves++;
+        }
+        UpdateWaveIndicator();
     }
 
     private async void NextWaveEvent()
     {
+        
         // check if player beat all the waves
         if (_waveIndex >= _wavesList.Length)
         {
@@ -60,6 +76,9 @@ public partial class WaveManager : Node
             return;
         }
 
+        // update wave indicator
+        UpdateWaveIndicator();
+        
         // spawn next enemy
         WaveEvent currentEvent = _wavesList[_waveIndex];
         if (currentEvent is Wave currentWave)
@@ -82,6 +101,7 @@ public partial class WaveManager : Node
                 // next waveevent
                 _wavePackIndex = 0;
                 _waveIndex++;
+                _currentWaveIndicatorNum++;
                 _enemyTimer.Start(_timeBetweenWaves);
             }
             else
@@ -96,6 +116,11 @@ public partial class WaveManager : Node
             currentLambda.Lambda.Invoke();
             _enemyTimer.Start(0);
         }
+    }
+
+    private void UpdateWaveIndicator()
+    {
+        GetNode<Label>("../WaveIndicator").Text = $"Wave {_currentWaveIndicatorNum}/{_totalWaves}";
     }
 }
 
